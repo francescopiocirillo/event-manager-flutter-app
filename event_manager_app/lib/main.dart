6af863864ae8cf83ce0b2546a554c970039434ec
francescopiocirillo <135717136+francescopiocirillo@users.noreply.github.com>
2024-05-06 12:56:39 +0200

    added the events list

--- event_manager_app/lib/main.dart
+++ event_manager_app/lib/main.dart
@@ -1,8 +1,30 @@
+import 'package:event_manager_app/ListTileExample.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
+class Event {
+  String title;
+  String desctiption;
+
+  Event({
+    required this.title,
+    required this.desctiption,
+  });
+}
+
+List<Event> events = [
+  Event(
+    title: 'Coachella',
+    desctiption: 'concert',
+  ),
+  Event(
+    title: 'Milano Fashon Week',
+    desctiption: 'parade',
+  ),
+];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -17,7 +39,16 @@ class MyApp extends StatelessWidget {
           title: Text('Event Manager'),
         ),
         body: SafeArea(
-          child: 
+          child: ListView.builder(
+            itemCount: events.length,
+            itemBuilder: (context, index) {
+              Event ev = events[index];
+              return ListTile(
+                  title: Text(ev.title), 
+                  subtitle: Text(ev.desctiption)
+              );
+            }
+          )
         ),
       ),
     );
